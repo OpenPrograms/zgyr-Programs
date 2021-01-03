2ef78c57e1cba1aff8c393a9d2860ff40d7b6618
zgyr <0112d759@gmail.com>
2021-01-03 17:20:33 +0600

    added lzss compression

--- bpacker/README.md
+++ bpacker/README.md
@@ -3,12 +3,17 @@
 A program for compressing and flashing files to EEPROM.
 
 
+## Requirements
+
+* Data Card any tier
+
+* Internet Card required for installation
+
+
 ## Download
 
 ```
-wget https://raw.githubusercontent.com/OpenPrograms/zgyr-Programs/master/installer.lua
-
-installer && rm installer.lua
+oppm install bpacker
 ```
 
 
@@ -23,6 +28,8 @@ Options:
   
   -m           minify code before compressing (unsafe)
   
+  -l, --lzss   use lzss algorithm for compression (no data card required)
+  
   -h, --help   display this help and exit
 ```
 
@@ -30,5 +37,5 @@ Options:
 ## TODO
 
 - [ ] Make a more cleverly packer
-- [ ] Add lz4 or lzss algorithm
+- [x] Add lz4 or lzss algorithm
 - [ ] Improve the minifier
--- bpacker/bpacker.lua
+++ bpacker/bpacker.lua
@@ -1,14 +1,16 @@
 local component = require('component')
+local computer = require('computer')
 local fs = require('filesystem')
 local shell = require('shell')
 local eeprom = component.eeprom
 
 local function usage()
-  print('Usage: compress [options] <filename>' .. [[
+  print('Usage: bpacker [options] <filename>' .. [[
   
 Options:
   -q           quiet mode, don't ask questions
   -m           minify code before compressing (unsafe)
+  -l, --lzss   use lzss algorithm for compression (no data card required)
   -h, --help   display this help and exit]])
 end
 
@@ -24,7 +26,12 @@ local function flash(str)
     print('Flashing EEPROM ' ..  eeprom.address)
     print('Please do NOT power down or restart your computer during this operation!')
   end
-  eeprom.set(str)
+  if #str < 4096 then
+    eeprom.set(str)
+  else
+    io.stderr:write('This file cannot compress to 4KiB\n')
+    return
+  end
   if not options.q then
     print('All done! You can remove the EEPROM and re-insert the previous one now.')
   end
@@ -43,11 +50,20 @@ local function deflate(str)
     return
   end
   result = decoder .. result .. ']==]'
-  if #result < 4096 then
-    flash(result)
-  else
-    io.stderr:write('This file cannot compress to 4KiB\n')
+  flash(result)
+end
+
+local function lzss(str)
+  if computer.getArchitecture() ~= 'Lua 5.3' then
+    io.stderr:write('This program requires Lua 5.3')
+    return
   end
+  local lzss_ = require('lzss')
+  local result = lzss_.getSXF(lzss_.compress(str))
+  if #result >= #str and not options.q then
+    print('Size of the compressed data is larger than the original.')
+  end
+  flash(result)
 end
 
 local filename = shell.resolve(args[1])
@@ -69,5 +85,9 @@ if options.m then
 end
 
 local f = io.open(filename)
-deflate(f:read('*a'))
+if options.lzss or options.l then
+  lzss(f:read('*a'))
+else
+  deflate(f:read('*a'))
+end
 f:close()

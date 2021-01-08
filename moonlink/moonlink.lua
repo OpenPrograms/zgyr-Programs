87d1c1aa25728585608c38530019798faefdd0fc
zgyr <0112d759@gmail.com>
2021-01-08 18:14:58 +0600

    moonlink fixes

--- moonlink/moonlink.lua
+++ moonlink/moonlink.lua
@@ -4,78 +4,76 @@ local event = require('event')
 
 local moonlink = {}
 
-local S_PORT, D_PORT = 8431, 8432
-local TIMER_INTERVAL = 12
+local PORT = 8431
+local TIMER_INTERVAL = 30
 local CODES = {
   ping = 'moonlink_ping',
   pong = 'moonlink_pong',
   send = 'moonlink_message'
 }
 local tmp = {}
-if not _G.moonlink_routes then
-  _G.moonlink_routes = {}
-  _G.moonlink_timers = {}
-  _G.moonlink_listeners = {}
+local moonlink_routes = {}
+
+if not moonlink_timers then
+  moonlink_timers = {}
+  moonlink_listeners = {}
 end
 
 local function listener(...)
   local e = {...}
-  if e[4] == S_PORT then
-    if e[6] == CODES.pong then
-      _, tmp = pcall(load(e[7]))
-      if type(tmp) == 'table' then
-        for address, jumps in pairs(tmp) do
-          if address and jumps and ((not _G.moonlink_routes[address]
-          or _G.moonlink_routes[address][1] > jumps)
-          and address ~= e[2]) then
-            _G.moonlink_routes[address] = {jumps + 1, e[3]}
-          end
+  if e[6] == CODES.pong then
+    _, tmp = pcall(load(e[7]))
+    if type(tmp) == 'table' then
+      for address, jumps in pairs(tmp) do
+        if (address and jumps and address ~= e[2])
+        and (not moonlink_routes[address]
+        or moonlink_routes[address][1] > jumps) then
+          moonlink_routes[address] = {jumps + 1, e[3]}
         end
       end
-    elseif e[6] == CODES.ping then
-      local result = 'return{'
-      for address, value in pairs(_G.moonlink_routes) do
-        result = result .. '["' .. address .. '"]=' .. value[1] .. ','
-      end
-      modem.send(e[3], S_PORT, CODES.pong, result .. '["' .. e[2] .. '"]=0}')
     end
-  elseif e[4] == D_PORT and e[6] == CODES.send then
-    if e[2] == e[6] then
+  elseif e[6] == CODES.ping then
+    local result = 'return{'
+    for address, value in pairs(moonlink_routes) do
+      result = result .. '["' .. address .. '"]=' .. value[1] .. ','
+    end
+    modem.send(e[3], PORT, CODES.pong, result .. '["' .. e[2] .. '"]=0}')
+  elseif e[6] == CODES.send then
+    if e[2] == e[7] then
       event.push(unpack(e, 6))
-    elseif _G.moonlink_routes[e[7]] then
-      modem.send(_G.moonlink_routes[e[7]][2], unpack(e, 6))
+    elseif moonlink_routes[e[7]] and moonlink_routes[e[7]] ~= e[2] then
+      modem.send(moonlink_routes[e[7]][2], PORT, unpack(e, 6))
     end
   end
 end
 
 moonlink.list = function()
   local list = {}
-  for address in pairs(_G.moonlink_routes) do
+  for address in pairs(moonlink_routes) do
     insert(list, address)
   end
   return list
 end
 
 moonlink.ping = function()
-  _G.moonlink_routes = {}
-  modem.broadcast(S_PORT, CODES.ping)
+  moonlink_routes = {}
+  modem.broadcast(PORT, CODES.ping)
 end
 
-moonlink.connect = function()
-  modem.open(S_PORT)
-  modem.open(D_PORT)
-  insert(_G.moonlink_timers, event.timer(TIMER_INTERVAL, moonlink.ping))
-  insert(_G.moonlink_listeners, event.listen('modem_message', listener))
+moonlink.connect = function(port)
+  PORT = PORT or port
+  modem.open(PORT)
+  insert(moonlink_timers, event.timer(TIMER_INTERVAL, moonlink.ping))
+  insert(moonlink_listeners, event.listen('modem_message', listener))
   moonlink.ping()
 end
 
 moonlink.disconnect = function()
-  modem.close(S_PORT)
-  modem.close(D_PORT)
-  if _G.moonlink_listeners then
-    for i = 1, #_G.moonlink_listeners do
-      event.cancel(_G.moonlink_listeners[i])
-      event.cancel(_G.moonlink_timers[i])
+  modem.close(PORT)
+  if moonlink_listeners then
+    for i = 1, #moonlink_listeners do
+      event.cancel(moonlink_listeners[i])
+      event.cancel(moonlink_timers[i])
     end
   else
     return false
@@ -83,8 +81,8 @@ moonlink.disconnect = function()
 end
 
 moonlink.send = function(address, ...)
-  if _G.moonlink_routes[address] then
-    modem.send(_G.moonlink_routes[address][2], D_PORT, CODES.send, address, ...)
+  if moonlink_routes[address] then
+    modem.send(moonlink_routes[address][2], PORT, CODES.send, address, ...)
   end
 end
 

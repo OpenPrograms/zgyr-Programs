8a3a8ffb3660482b8e41e62c58127f9c89363da9
zgyr <0112d759@gmail.com>
2021-01-21 12:20:01 +0600

    J1 decoder

--- heap_of_VMs/J1/main.lua
+++ heap_of_VMs/J1/main.lua
@@ -28,3 +28,27 @@ local CPU = {
   T = 0, -- d_stack pointer
   R = 0, -- r_stack pointer
 }
+
+local function decoder(IR)
+  if IR & 0x8000 == 0x8000 then -- LIT
+    CPU.T = CPU.T + 1 % CONST.STACK_SIZE
+    D_STACK[CPU.T] = IR & 0x7FFF
+    CPU.PC = CPU.PC + 1 % CONST.MEM_SIZE
+  else
+    local typ, IR = IR & 0xE0000, IR & 0x1FFF
+    if typ == 0 then -- JMP
+      CPU.PC = IR
+    elseif typ == 0x2000 then -- JMPZ
+      if CPU.D_STACK[CPU.T] == 0 then
+        CPU.PC = IR
+      end
+      CPU.T = CPU.T + 1 % CONST.STACK_SIZE
+    elseif typ == 0x4000 then -- CALL
+      CPU.R = CPU.R + 1 % CONST.STACK_SIZE
+      CPU.R_STACK[CPU.R] = CPU.PC + 1 % CONST.MEM_SIZE
+      CPU.PC = IR
+    elseif typ == 0x6000 then -- ALU
+      ALU(IR)
+    end
+  end
+end

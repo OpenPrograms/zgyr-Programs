dcb3967555327ceef8f463272757a4c6188d83dc
zgyr <0112d759@gmail.com>
2021-01-23 21:24:46 +0600

    J1 circular stack and memory, to be or not to be?

--- heap_of_VMs/J1/main.lua
+++ heap_of_VMs/J1/main.lua
@@ -7,6 +7,8 @@ fedc ba98 7654 3210
 011T TTTT XXXX RRDD alu
 ]]
 
+min, max = math.min, math.max
+
 local function int16(n)
   return n - (n >> 1) & 0xffff
 end
@@ -30,6 +32,15 @@ local CPU = {
   T = 0, -- d_stack pointer
   R = 0, -- r_stack pointer
 }
+
+local function init()
+  for i = 0, CONST.STACK_SIZE do
+    D_STACK, R_STACK = 0, 0
+  end
+  for i = 0, CONST.MEM_SIZE do
+    CPU.MEMORY = 0
+  end
+end
 
 local function TD() -- top of d_stack
   return CPU.D_STACK[CPU.T]
@@ -60,15 +71,15 @@ local OP = {
 
 local function ALU(IR)
   if IR & 0x3 then -- DS +-
-    CPU.D_STACK[CPU.T] = TD() + 1 % CONST.STACK_SIZE
+    CPU.D_STACK[CPU.T] = min(TD() + 1, CONST.STACK_SIZE)
   elseif RS == 2 then
-    CPU.D_STACK[CPU.T] = TD() - 1 % CONST.STACK_SIZE
+    CPU.D_STACK[CPU.T] = max(TD() - 1, 0)
   end
   local RS = (IR & 0xC) >> 2
   if RS == 1 then -- RS +-
-    CPU.R_STACK[CPU.R] = CPU.R_STACK[CPU.R] + 1 % CONST.STACK_SIZE
+    CPU.R_STACK[CPU.R] = min(CPU.R_STACK[CPU.R] + 1, CONST.STACK_SIZE)
   elseif RS == 2 then
-    CPU.R_STACK[CPU.R] = CPU.R_STACK[CPU.R] - 1 % CONST.STACK_SIZE
+    CPU.R_STACK[CPU.R] = max(CPU.R_STACK[CPU.R] - 1, 0)
   end
   local result = 0
   if OP[IR & 0x1F00 >> 8] then -- OP
@@ -78,22 +89,22 @@ local function ALU(IR)
     CPU.PC = CPU.R_STACK[CPU.R]
   end
   if IR & 0x40 == 1 then -- T -> N
-    CPU.D_STACK[CPU.T + 1 % CONST.STACK_SIZE] = TD()
+    CPU.D_STACK[min(CPU.T + 1, 0)] = TD()
   end
   if IR & 0x20 == 1 then -- T -> R
     CPU.R_STACK[CPU.R] = TD()
   end
   if IR & 0x10 == 1 then -- N -> [T]
-    CPU.MEMORY[TD()] = CPU.D_STACK[CPU.T - 1 % CONST.STACK_SIZE]
+    CPU.MEMORY[TD()] = ND()
   end
   CPU.D_STACK[CPU.T] = result
 end
 
 local function decoder(IR)
   if IR & 0x8000 == 0x8000 then -- LIT
-    CPU.T = CPU.T + 1 % CONST.STACK_SIZE
+    CPU.T = min(CPU.T + 1, CONST.STACK_SIZE)
     D_STACK[CPU.T] = IR & 0x7FFF
-    CPU.PC = CPU.PC + 1 % CONST.MEM_SIZE
+    CPU.PC = min(CPU.PC + 1, CONST.MEM_SIZE)
   else
     local typ, IR = IR & 0xE0000, IR & 0x1FFF
     if typ == 0 then -- JMP
@@ -105,7 +116,7 @@ local function decoder(IR)
       CPU.T = CPU.T + 1 % CONST.STACK_SIZE
     elseif typ == 0x4000 then -- CALL
       CPU.R = CPU.R + 1 % CONST.STACK_SIZE
-      CPU.R_STACK[CPU.R] = CPU.PC + 1 % CONST.MEM_SIZE
+      CPU.R_STACK[CPU.R] = min(CPU.PC + 1, CONST.MEM_SIZE)
       CPU.PC = IR
     elseif typ == 0x6000 then -- ALU
       ALU(IR)

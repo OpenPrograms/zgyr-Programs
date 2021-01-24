0440122f2580535b7e278c96fc7c12820d998187
zgyr <0112d759@gmail.com>
2021-01-24 12:32:14 +0600

    J1 %define YAY!

--- heap_of_VMs/J1/assembler.lua
+++ heap_of_VMs/J1/assembler.lua
@@ -30,7 +30,7 @@
 1000 0000 0000 0000 LIT
 --]]
 
-local string, tonumber = string, tonumber
+local char, tonumber = string.char, tonumber
 local args = {...}
 if #args ~= 1 then
   print('Usage: assembler <file.j1>')
@@ -40,10 +40,10 @@ else
 end
 
 local opcodes = {
-  ['d--']    = 0x0001,
-  ['d++']    = 0x0002,
-  ['r--']    = 0x0004,
-  ['r++']    = 0x0008,
+  ['D--']    = 0x0001,
+  ['D++']    = 0x0002,
+  ['R--']    = 0x0004,
+  ['R++']    = 0x0008,
   ['N->[T]'] = 0x0010,
   ['T->R']   = 0x0020,
   ['T->N']   = 0x0040,
@@ -62,9 +62,11 @@ local alu = {
   '[T]', 'N<<T', 'DSP', 'Nu<T'
 }
 
-for i = 1, #alu do
+for i = 0, #alu do
   opcodes[alu[i]] = i << 9
 end
+
+local definitions = {}
 
 local program = ''
 
@@ -74,18 +76,40 @@ f:close()
 
 for line in data:gmatch('[^\n]+') do
   local result = 0
+  local counter = 0
+  local def = false
+  local name = nil
   for code in line:gmatch('[^%s]+') do
-    if opcodes[code:upper()] then
-      result = result | opcodes[code:upper()]
-    elseif tonumber(code) then
-      result = result | tonumber(code)
-    elseif code == '//' then
-      break
+    counter = counter + 1
+    if code == '%define' and counter == 1 then
+      def = true
+    elseif def and counter == 2 then
+      name = code
     else
-      print('UNKNOWN WORD: ' .. code)
+      if opcodes[code:upper()] then
+        result = result | opcodes[code:upper()]
+      elseif tonumber(code) then
+        result = result | tonumber(code)
+      elseif code == '//' then
+        break
+      elseif definitions[code] then
+        result = definitions[code]
+        break
+      else
+        print('UNKNOWN WORD: ' .. code)
+      end
     end
   end
-  program = program .. string.char(result & 0xFF00) .. string.char(result & 0x00FF)
+  if type(result) ~= 'string' then
+    result = char((result & 0xFF00) >> 8) .. char(result & 0x00FF)
+  end
+  if def then
+    definitions[name] = result
+  else
+    if type(result) == 'string' then
+      program = program .. result
+    end
+  end
 end
 
 f = io.open(filename .. '.ac', 'w')
